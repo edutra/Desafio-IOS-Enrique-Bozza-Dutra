9888dab2f977c7eb9e510aba25d0de4b16eb64d9
edutra <edutra@gmail.com>
2021-09-04 20:11:38 -0300

    SegmentedControl and table View Created

--- GitHubReader/GitHubReader/View/ViewController.swift
+++ GitHubReader/GitHubReader/View/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let segmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Item 1", "item 2"])
+        return sc
+    }()
+    
+    let tableView = UITableView(frame: .zero, style: .plain)
+    
     override func loadView() {
         let view = UIView(frame: UIScreen.main.bounds)
         view.backgroundColor = .white
         self.view = view
-        navigationItem.title = "Github Repos"
+        navigationItem.title = "Github Repos "
     }
 
     override func viewDidLoad() {
