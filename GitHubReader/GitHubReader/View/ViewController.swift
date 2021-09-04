f95b9574296b13a5098b5c780050f0d946d45faa
edutra <edutra@gmail.com>
2021-09-04 20:26:08 -0300

    Segmented control and table View added to the view

--- GitHubReader/GitHubReader/View/ViewController.swift
+++ GitHubReader/GitHubReader/View/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     let segmentedControl: UISegmentedControl = {
         let sc = UISegmentedControl(items: ["Item 1", "item 2"])
+        sc.selectedSegmentIndex = 0
         return sc
     }()
     
@@ -21,13 +22,26 @@ class ViewController: UIViewController {
         let view = UIView(frame: UIScreen.main.bounds)
         view.backgroundColor = .white
         self.view = view
-        navigationItem.title = "Github Repos "
+        navigationItem.title = "Github Repos"
+        
+        
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            segmentedControl, tableView
+            ])
+        
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+//        view.addSubview(segmentedControl)
+        stackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .zero)
+        
     }
 
 
