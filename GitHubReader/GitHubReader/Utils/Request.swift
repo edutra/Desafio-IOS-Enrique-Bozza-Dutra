bb91341689e113d3e22b56de83d4f4ece6e4cfaf
edutra <edutra@gmail.com>
2021-09-06 13:38:22 -0300

    request class started

--- GitHubReader/GitHubReader/Utils/Request.swift
+++ GitHubReader/GitHubReader/Utils/Request.swift
@@ -10,4 +10,17 @@ import Foundation
 
 class Request{
     
+    func repoRequest(){
+//        let url = URL(string: "http://api.github.com/search/repositories?q=language:Swift&sort=stars")
+        
+        guard let url = URL(string: "http://api.github.com/search/repositories?q=language:Swift&sort=stars") else { return }
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            
+        }
+    }
+    
+    func pullRequest(){
+        
+    }
+    
 }
